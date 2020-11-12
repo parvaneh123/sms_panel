50139809b9c965d72b18f34d6c63f39c814fa28b
parvanehpasha <parvaneh.moradipasha@gmail.com>
2020-11-13 00:54:09 +0330

    create model and admin

--- smsapp/admin.py
+++ smsapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomerGroups,Customer
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['phone_number','name','company']
 
 # Register your models here.
+admin.site.register(Customer,CustomerAdmin)
+admin.site.register(CustomerGroups)
--- smsapp/models.py
+++ smsapp/models.py
@@ -1,3 +1,20 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class CustomerGroups(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name_group = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name_group
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.IntegerField()
+    company = models.CharField(max_length=100)
+    group_customer = models.ForeignKey(CustomerGroups, on_delete=models.CASCADE)
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
