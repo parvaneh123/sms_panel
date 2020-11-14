5e8985c53faecfb2083514a791d6d7d5c02ed325
parvanehpasha <parvaneh.moradipasha@gmail.com>
2020-11-14 19:51:38 +0330

    change model and admin

--- smsapp/admin.py
+++ smsapp/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import CustomerGroups,Customer
+from .models import CustomerGroups, Customer
 
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['phone_number','name','company']
+    list_display = ['phone_number', 'name', 'company', 'unique_id']
 
-# Register your models here.
-admin.site.register(Customer,CustomerAdmin)
-admin.site.register(CustomerGroups)
+
+class CustomerGroupAdmin(admin.ModelAdmin):
+    list_display = ['name_group', 'owner', 'unique_group', 'id']
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(CustomerGroups, CustomerGroupAdmin)
+
--- smsapp/models.py
+++ smsapp/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 class CustomerGroups(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name_group = models.CharField(max_length=50)
+    id = models.AutoField(primary_key=True)
+    unique_group = models.UUIDField(default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return self.name_group
@@ -13,8 +15,8 @@ class CustomerGroups(models.Model):
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
-    phone_number = models.IntegerField()
+    phone_number = models.CharField(max_length=15)
     company = models.CharField(max_length=100)
     group_customer = models.ForeignKey(CustomerGroups, on_delete=models.CASCADE)
-    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    unique_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
